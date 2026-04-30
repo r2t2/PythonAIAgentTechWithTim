75cd51a109fb6e6ec0252e10fab464a4e0bf3700
Roger Tyson <tysonrj@gmail.com>
2026-04-30 16:56:07 -0500

    Mid project
    Checking in the code due to issues.  Parameters and functions appear to have changed from video creation to now. Will have to research differences and get it figured out.

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_agent
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+load_dotenv()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    source: list[str]
+    tools_used: list[str]
+
+
+# llm = ChatOpenAI(model="gpt-40-mini")
+# llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+llm = ChatAnthropic(model="claude-sonnet-4-6")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+prompt2 = SystemMessagePromptTemplate.fr(
+    [
+        (
+            "system",
+            """
+            """
+        )
+    ]
+)
+
+agent = create_agent(
+    tools=[],
+    system_prompt=prompt,
+)
